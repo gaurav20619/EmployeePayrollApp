25f79ffd1d2ab8f68f84485b38228a52ddd6065f
gaurav20619 <gauravsinghraijada619@gmail.com>
2023-04-01 06:58:15 +0530

    Add: Ability To add from local storage

--- homePage.js
+++ homePage.js
@@ -1,14 +1,23 @@
+let empPayrollList;
 window.addEventListener('DOMContentLoaded', (event) => {
+    empPayrollList = getEmployeePayrollDataFromStorage();
+    document.querySelector(".emp-count").textContent = empPayrollList.length;
     createInnerHtml();
+    localStorage.removeItem('editEmp');
 });
 
+const getEmployeePayrollDataFromStorage = () => {
+    return localStorage.getItem('EmployeePayrollList') ?
+        JSON.parse(localStorage.getItem('EmployeePayrollList')) : [];
+}
 
 const createInnerHtml = () => {
+    if (empPayrollList.length == 0) return;
     const headerHtml = "<th></th><th>Name</th><th>Gender</th>" +
         "<th>Department</th><th>Salary</th><th>Start Date</th>" +
         "<th>Actions</th>";
     let innerHtml = `${headerHtml}`;
-    let empPayrollList = createEmployeePayrollJSON();
+    //let empPayrollList = createEmployeePayrollJSON();  
     for (const empPayrollData of empPayrollList) {
         innerHtml = `${innerHtml}
         <tr>
@@ -21,9 +30,9 @@ const createInnerHtml = () => {
                 ${getDeptHtml(empPayrollData._department)}
             </td>
             <td>${empPayrollData._salary}</td>
-            <td>${empPayrollData._start_date}</td>
+            <td>${stringifyDate(empPayrollData._start_date)}</td>
             <td>
-                <img id="1" onclick="remove(this)" alt="delete"
+                <img id="${empPayrollData._id}" alt="delete"
                             src="../assets/icons/delete-black-18dp.svg">
                 <img id="1" onclick="update(this)" alt="edit"
                             src="../assets/icons/create-black-18dp.svg">           
@@ -42,21 +51,22 @@ const getDeptHtml = (deptList) => {
     return deptHtml;
 }
 
+
 const createEmployeePayrollJSON = () => {
     let empPayrollListLocal = [
         {
-            _name: 'Gaurav singh',
+            _name: 'Arindam Saha',
             _gender: 'Male',
             _department: [
-                'Full stack','Developer', 'Engineer'
+                'Full stack','Java Developer', 'Engineer'
             ],
             _salary: '500000',
-            _start_date: '21 Oct 2022',
+            _start_date: '15 Jul 2022',
             _note: '',
-            _profilePic: '../assets/profile-images/Ellipse -8.png'
+            _profilePic: 'assets/profile-images/Ellipse -3.png',
         },
         {
-            _name: 'selina',
+            _name: 'Regina',
             _gender: 'Female',
             _department: [
                'Sales','Finance', 'HR'
@@ -64,7 +74,7 @@ const createEmployeePayrollJSON = () => {
             _salary: '300000',
             _start_date: '01 Nov 2022',
             _note: '',
-            _profilePic: '../assets/profile-images/Ellipse 1.png'
+            _profilePic: 'assets/profile-images/Ellipse -4.png'        
         }
     ]
     return empPayrollListLocal;
